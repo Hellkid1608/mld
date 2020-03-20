b3b9b8062bac4e547f8a9786badc392084efc1b0
Uriel <uriel.nava@busolinea.com>
2020-03-20 16:01:43 -0600

    schedule backend Mld

---
+++ users/user.service.js
@@ -0,0 +1,62 @@
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+
+module.eports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function authenticate({ username, password }) {
+    const user = await user.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash )){
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id }, config.secret);
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+}
+
+async function getAll() {
+    return await User.find().select('-hash');
+}
+
+async function getById(id) {
+    return await User.findById(id).select('-hash');
+}
+
+async function create(userParam) {
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    await user.save();
+}
+
+async function update(id, userParam) {
+    const user = await User.findById(id);
+
+    if(!user) throw 'User not found';
+    if (user.name !== userParam.username && await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    if (userParam.password) {
+        userParam.hash = bcrypt.hashSync(userParam.password,10);
+    }
+
+    Object.assign(user, userParam);
+
+    await user.save();
+}
+
+async function _delete(id) {
+    await User.findByIdAndRemove(id);
+}
